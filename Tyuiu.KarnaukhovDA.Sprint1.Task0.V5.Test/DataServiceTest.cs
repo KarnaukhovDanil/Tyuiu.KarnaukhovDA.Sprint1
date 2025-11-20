5147f7af48a374c9c1fb6ac09243deeeb78e924a
Даниил Карнаухов <Tyuiu.KarnaukhovDA@outlook.com>
2025-11-20 18:37:43 +0500

    Add project files.

---
+++ Tyuiu.KarnaukhovDA.Sprint1.Task0.V5.Test/DataServiceTest.cs
@@ -0,0 +1,15 @@
+﻿using Tyuiu.KarnaukhovDA.Sprint1.Task0.V5.Lib;
+namespace Tyuiu.KarnaukhovDA.Sprint1.Task0.V5.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            var res = ds.Calculate();
+            Assert.AreEqual(12, res);
+        }
+    }
+}
