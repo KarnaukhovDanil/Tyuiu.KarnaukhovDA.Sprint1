15d04171cd2ce3dd29b2a152cc06f4dd37b98b78
Даниил Карнаухов <Tyuiu.KarnaukhovDA@outlook.com>
2025-11-20 22:52:59 +0500

    Task3

---
+++ Tyuiu.KarnaukhovDA.Sprint1.Task3.V13.Lib/DataService.cs
@@ -0,0 +1,11 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint1;
+namespace Tyuiu.KarnaukhovDA.Sprint1.Task3.V13.Lib
+{
+    public class DataService : ISprint1Task3V13
+    {
+        public double MultiplyOfDigits(double number)
+        {
+            return number * number;
+        }
+    }
+}
---
+++ Tyuiu.KarnaukhovDA.Sprint1.Task3.V13.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.KarnaukhovDA.Sprint1.Task3.V13.Lib;
+namespace Tyuiu.KarnaukhovDA.Sprint1.Task3.V13.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            double number = 13;
+            
+            double wait = 169;
+            var res = ds.MultiplyOfDigits(number);
+            Assert.AreEqual(wait, res, 0.001);
+        }
+    }
+}
