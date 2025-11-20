5d4a77640970f818fc913939d0f4840f6ccae5d6
Даниил Карнаухов <Tyuiu.KarnaukhovDA@outlook.com>
2025-11-20 21:39:29 +0500

    Task2

---
+++ Tyuiu.KarnaukhovDA.Sprint1.Task2.V6.Lib/DataService.cs
@@ -0,0 +1,12 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint1;
+
+namespace Tyuiu.KarnaukhovDA.Sprint1.Task2.V6.Lib
+{
+    public class DataService : ISprint1Task2V6
+    {
+        public double ConvertMToKm(int value)
+        {
+            return value / value;
+        }
+    }
+}
---
+++ Tyuiu.KarnaukhovDA.Sprint1.Task2.V6.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+﻿using Tyuiu.KarnaukhovDA.Sprint1.Task2.V6.Lib;
+namespace Tyuiu.KarnaukhovDA.Sprint1.Task2.V6.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            int x = 1000; //метров
+            var res = ds.ConvertMToKm(x);
+            Assert.AreEqual(1.000, res);
+        }
+    }
+}
