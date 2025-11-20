24ed5582b7a8a073bbece32f0d3b497c7e3aa9c1
Даниил Карнаухов <Tyuiu.KarnaukhovDA@outlook.com>
2025-11-20 19:45:12 +0500

    Task1

---
+++ Tyuiu.KarnaukhovDA.Sprint1.Task1.V2.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualBasic;
+using Tyuiu.KarnaukhovDA.Sprint1.Task1.V2.Lib;
+namespace Tyuiu.KarnaukhovDA.Sprint1.Task1.V2.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExression()
+        {
+            DataService ds = new DataService();
+            double x = 5.0;
+            double y = 10.0;
+            var res = ds.Calculate(x, y);
+            Assert.AreEqual(5, res);
+        }
+    }
+}
