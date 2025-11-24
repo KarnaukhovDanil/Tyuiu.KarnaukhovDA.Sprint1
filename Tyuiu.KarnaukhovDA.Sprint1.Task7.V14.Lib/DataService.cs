52af4f03d2d21bb7a8fc155068e6cbf1917f3197
Даниил Карнаухов <Tyuiu.KarnaukhovDA@outlook.com>
2025-11-24 16:39:24 +0500

    Task7

--- Tyuiu.KarnaukhovDA.Sprint1.Task7.V14.Lib/DataService.cs
+++ Tyuiu.KarnaukhovDA.Sprint1.Task7.V14.Lib/DataService.cs
@@ -10,7 +10,8 @@ namespace Tyuiu.KarnaukhovDA.Sprint1.Task7.V14.Lib
     {
         public double Calculate(double x, double y)
         {
-            double res = Math.Pow(2, -x) + 5 * Math.Pow(x, 2) / 3 * Math.Pow(x, 3) - Math.Pow(Math.Cos(x), 2) + Math.Sin(2 * x * y);
+            double res = Math.Pow(2, -x) + (5 * Math.Pow(x, 2)) / (3 * Math.Pow(x, 3)) - Math.Cos(Math.Pow(x, 2)) + Math.Sin(2 * x * y);
+            
             return res;
         }
     }
--- Tyuiu.KarnaukhovDA.Sprint1.Task7.V14.Test/DataServiceTest.cs
+++ Tyuiu.KarnaukhovDA.Sprint1.Task7.V14.Test/DataServiceTest.cs
@@ -9,9 +9,9 @@ namespace Tyuiu.KarnaukhovDA.Sprint1.Task7.V14.Test
         public void ValidExpression()
         {
             DataService ds = new DataService();
-            double x = 1;
-            double y = 2;
-            double wait = 1.118;
+            double x = 5;
+            double y = 5;
+            double wait = -0.889;
             var res = ds.Calculate(x, y);
             Assert.AreEqual(wait, res, 0.001);
         }
